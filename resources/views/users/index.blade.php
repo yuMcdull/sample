18cf37e14dfba5e93c2331a3d194e15610d507d3
ybb <1319933170@qq.com>
2018-03-22 05:24:05 +0000

    finish user edit,

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.default')
+@section('title', '所有用户')
+
+@section('content')
+<div class="col-md-offset-2 col-md-8">
+  <h1>所有用户</h1>
+  <ul class="users">
+    @foreach ($users as $user)
+      <li>
+        <img src="{{ $user->gravatar() }}" alt="{{ $user->name }}" class="gravatar"/>
+        <a href="{{ route('users.show', $user->id )}}" class="username">{{ $user->name }}</a>
+      </li>
+      @can('destory',$user)
+      	<form action="{{route('users.destory',$user->id)}}" method="post">
+      		{{ csrf_field() }}
+      		{{ method_field('DELETE') }}
+      		 <button type="submit" class="btn btn-sm btn-danger delete-btn">删除</button>
+      	</form>
+      @endcan
+    @endforeach
+  </ul>
+  {!! $users->render() !!}
+</div>    
+@stop
